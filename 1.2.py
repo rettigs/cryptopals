563873e8feea90c4c26a847928d686ed31e2e2f8
Sean Rettig <seanrettig@gmail.com>
2014-12-10 15:03:25 -0800

    1.2 complete

---
+++ 1.2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# http://cryptopals.com/sets/1/challenges/2/
+
+def hex_xor(a, b):
+    '''Given two hex strings, bitwise XORs them and returns the result.'''
+    #return operator.xor(*map(int, sys.argv[1:3], [16]*2))
+    a, b = map(int, sys.argv[1:3], [16]*2)
+    return a ^ b
+
+if __name__ == '__main__':
+    import sys
+    print hex_xor(*sys.argv[1:3])
