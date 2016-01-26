7232c93b97c54bcfd53a2d28b8452a4577bcb940
Sean Rettig <seanrettig@gmail.com>
2016-01-25 17:25:48 -0800

    1.2: fix formatting; actually output in hex

--- 1.2.py
+++ 1.2.py
@@ -6,7 +6,7 @@ def hex_xor(a, b):
     '''Given two hex strings, bitwise XORs them and returns the result.'''
     #return operator.xor(*map(int, sys.argv[1:3], [16]*2))
     a, b = map(int, sys.argv[1:3], [16]*2)
-    return a ^ b
+    return "{:x}".format(a ^ b)
 
 if __name__ == '__main__':
     import sys
